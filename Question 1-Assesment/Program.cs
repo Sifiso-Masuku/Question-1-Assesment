4802c2b2ee9b1d638ba32bbc67bab91cf013b5ae
LENOVO <LENOVO@DESKTOP-6092P3H>
2022-07-04 12:16:10 +0200

    Add project files.

---
+++ Question 1-Assesment/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 130; i++)
+        {
+            if(i % 3 == 0) 
+            {
+                Console.WriteLine("Hot");
+            }
+            else if(i % 5 == 0)
+            {
+                Console.WriteLine( "Chocolate");
+            }
+            else
+            { Console.WriteLine(i); }
+
+
+        }
+        Console.ReadKey();
+    }
+}
